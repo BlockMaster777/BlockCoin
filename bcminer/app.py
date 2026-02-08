0a8a2493313f68a89c8b46e688361a4a23e3e33a
BlockMaster <blockmaster350@gmail.com>
2026-02-08 16:06:03 +0300

    Made GUI layout

--- bcminer/app.py
+++ bcminer/app.py
@@ -1,14 +1,37 @@
 # coding=utf-8
 import tkinter as tk
+from tkinter import ttk
 
 
-def main() -> None:
-    app = tk.Tk()
-    app.title("BlockCoinMiner")
-    app.configure(width=700, height=500, bg="#797979")
-    app.resizable(width=False, height=False)
-    app.iconbitmap("icon.ico")
-    app.mainloop()
+app = tk.Tk()
+app.title("BlockCoinMiner")
+app.configure(width=700, height=500)
+app.resizable(width=False, height=False)
+app.iconbitmap("icon.ico")
 
-if __name__ == "__main__":
-    main()
+ttk.Style().configure("TButton", font=("Arial", 14))
+
+def validate_input(p):
+    if p.isdigit():
+        return True
+    return False
+
+ttk.Label(app, text="Owner", font=("Arial", 14)).place(x=20, y=20)
+owner_var = tk.StringVar(app)
+ttk.Entry(app, font=("Arial", 14), width=30, textvariable=owner_var).place(x=20, y=55)
+
+ttk.Label(app, text="Count", font=("Arial", 14)).place(x=20, y=90)
+count_var = tk.IntVar(app)
+ttk.Entry(app, font=("Arial", 14), width=30, validatecommand=(app.register(validate_input), "%P"),
+          validate="key", textvariable=count_var).place(x=20, y=125)
+
+ttk.Button(app, text="Start Mining").place(x=20, y=170)
+
+ttk.Label(app, text="Current Tokens", font=("Arial", 14)).place(x=200, y=180)
+current_list = tk.Listbox(app, font=("Arial", 5), width=83, height=30)
+current_list.place(x=20, y=220)
+
+ttk.Label(app, text="All tokens", font=("Arial", 14)).place(x=380, y=20)
+all_list = tk.Listbox(app, font=("Arial", 5), width=70, height=50)
+all_list.place(x=380, y=60)
+app.mainloop()
